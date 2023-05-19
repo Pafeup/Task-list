b7c312d5fef8ccb64390be9ea1a8903eb586835e
Paweł Pełka <pinio.pelka@gmail.com>
2023-05-19 19:35:09 +0200

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [
+        {
+            content: "first task",
+            done: true,
+        },
+        {
+            content: "second taks",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+    
+    const init = () => {
+        render();
+    };
+
+    init();
+};
