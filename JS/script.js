c9191edc152f61d75fd8fcaf39d5e1bf24ddc010
Paweł Pełka <pinio.pelka@gmail.com>
2023-05-21 14:08:26 +0200

    added removeTask and toggleTaskDone functions

--- JS/script.js
+++ JS/script.js
@@ -10,26 +10,55 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li>
-                ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-taskList").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
         render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const toggleTaskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render();
     }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="taskList__item">
+            <button class="taskList__Button--done js-done">O</button>
+                <span class=${task.done ? "taskList__done" : ""}>${task.content}</span>
+            <button class="taskList__Button--delete js-remove">X</button>    
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-taskList").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        console.log(toggleDoneButtons)
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -41,7 +70,7 @@
         }
 
         addNewTask(newTaskContent);
-    }
+    };
 
     const init = () => {
         render();
